2af416aa6d9bd097b2823dc5f0a27284d06c0f5c
max-gow <basarskyu.oleg@gmail.com>
2021-01-24 11:23:20 +0200

    24.01.21

--- src/AreaSquare.java
+++ src/AreaSquare.java
@@ -1,3 +1,4 @@
+//Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
 public class AreaSquare {
     public static void main(String[] args) {
         //System.out.printf("%.2f", squareArea(14.05)); // обрізає до 2 знаків після коми
--- src/StringRepeat.java
+++ src/StringRepeat.java
@@ -1,5 +1,20 @@
+//Write a function called repeat_str which repeats the given string src exactly count times.
 public class StringRepeat {
     public static void main(String[] args) {
+        System.out.println(repeatStr(4, "Hello"));
 
+    }
+    public static String repeatStr(final int repeat, final String string) {
+        /*String res = "";
+        for (int i = 0; i < repeat; i++){   //не очень хорошая практика
+            res = res + string;
+        }
+        return res;*/
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < repeat; i++) {
+            sb.append(string);
+        }
+        return sb.toString();
     }
 }
